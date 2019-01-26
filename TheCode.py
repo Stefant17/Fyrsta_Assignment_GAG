05f5291a14df8dc0fae0380c369a0a8fcdbb2fce
stebbi <stefant17@ru.is>
2019-01-26 12:24:08 +0000

    added funcitonality

--- TheCode.py
+++ TheCode.py
@@ -3,3 +3,39 @@ class Arraylist:
     self.size = 0
     self.capacity = 4
     self.arr = [0] * self.capacity
+
+  def print(self):
+    for i in range(self.size):
+      print(self.arr[i]
+
+  def prepend(self, value):
+    if(capacity <= size):
+      resize(self)
+    for i in range(self.size):
+      self.arr[i] = self.arr[i+1]
+    return 0
+
+  def insert(self, value, index):
+    
+    return 0
+
+  def append(self, value):
+    return 0
+
+  def set_at(self, value, index):
+    return 0
+
+  def get_first(self):
+    return 0
+
+  def get_last(self): 
+    return 0
+
+  def resize(self): 
+    return 0
+
+  def remove_at(self, index):
+    return 0
+
+  def clear(self):
+    return 0
